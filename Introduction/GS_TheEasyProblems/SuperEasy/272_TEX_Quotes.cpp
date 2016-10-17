6cd1aa25d47edd84bbd79d38cc9a221d398212c6
samirtf <samircc20092@gmail.com>
2016-10-17 14:09:23 -0300

    Adding result to UVaquestion 272

--- Introduction/GS_TheEasyProblems/SuperEasy/272_TEX_Quotes.cpp
+++ Introduction/GS_TheEasyProblems/SuperEasy/272_TEX_Quotes.cpp
@@ -10,6 +10,15 @@ License: Apache 2.0
 272 - TEX Quotes
 Time limit: 3.000 seconds
 https://uva.onlinejudge.org/index.php?option=onlinejudge&page=show_problem&problem=208
+
+Id:18205826
+Code:272
+Problem: TEX Quotes
+Verdict: Accepted
+Language: C++
+Run Time: 0.000
+Submission Date:2016-10-17 17:06:27
+
 */
 int main() {
 	
