72ffad65c177148bdbe3df927620163972585f6d
samirtf <samircc20092@gmail.com>
2016-10-24 14:05:15 -0300

    UVA - 1124 - Celebrity jeopardy - Accepted

---
+++ Introduction/GS_TheEasyProblems/SuperEasy/1124_Celebrity_jeopardy.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+/*
+Author: Samir Trajano Feitosa
+Github: https://github.com/samirtf
+License: Apache 2.0
+
+1124 - Celebrity jeopardy
+Time limit: 3.000 seconds
+https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=3565
+
+Id:18248984
+Code:1124
+Problem: Celebrity jeopardy
+Verdict: Accepted
+Language: C++
+Run Time: 0.000
+Submission Date:2016-10-24 17:03:17
+
+*/
+int main() {
+	char c;
+	while(scanf("%c", &c) != EOF) {
+		printf("%c", c);
+	}
+	return 0;
+}
